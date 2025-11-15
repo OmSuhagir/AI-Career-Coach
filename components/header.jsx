566aa8aadd40bc64a5db68e7263927465d8cd261
OmSuhagir <omsuhagir123@gmail.com>
2025-11-15 21:03:14 +0530

    logo change

--- components/header.jsx
+++ components/header.jsx
@@ -14,7 +14,7 @@ const Header = async () => {
     <header className='fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60 '>
       <nav className='container mx-auto px-4 h-16 flex items-center justify-between'>
         <Link href="/">
-          <Image src='/logo.png' alt='sensai logo' width={200} height={200} className='h-12 py-1 w-auto object-contain' />
+          <Image src='/newLogo.png' alt='sensai logo' width={200} height={200} className='h-48 py-1 w-auto object-contain' />
         </Link>
 
 
